98445c4bb578c3be7cb198262783ec214216598b
Caio Ferraz <caioferrazalmeida.27@gmail.com>
2023-09-25 12:09:23 -0300

    feat(home): add dynamic data with firebase

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,17 +1,52 @@
+import { collection, onSnapshot } from "firebase/firestore";
+import { useEffect, useState } from "react";
 import { FlatList, SafeAreaView, StyleSheet, Text, View } from "react-native";
 import Line from "../components/lineSeparator";
+import { db } from "../config/firebase";
 
 const Home = () => {
+
+  const [totalProducts, setTotalProducts] = useState(0);
+  const [totalProductsValue, setTotalProductsValue] = useState(0);
+  const [totalSalesCurrentMonth, setTotalSalesCurrentMonth] = useState(0);
+  const [totalSalesValue, setTotalSalesValue] = useState(0);
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  const loadData = () => {
+    try {
+      const product = onSnapshot(collection(db, "produtos"), (DATA) => {
+        DATA.forEach((doc) => {
+          setTotalProducts((prev) => parseInt(prev + doc.data().quantity));
+          setTotalProductsValue((prev) => prev + (doc.data().quantity * doc.data().price));
+          console.log(totalProductsValue);
+          });
+        });
+        const sales = onSnapshot(collection(db, "vendas"), (DATA) => {
+          DATA.forEach((doc) => {
+            setTotalSalesValue((prev) => prev + doc.data().value);
+            setTotalSalesCurrentMonth((prev) => prev + doc.data().value)
+        });
+      });
+
+    } catch (error) {
+      alert("Não foi possível carregar os itens!");
+      console.log(error);
+    }
+  };
+
   const DATA = [
     {
       id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
       title: "Produtos em estoque",
-      content: "10",
+      content: totalProducts,
     },
     {
       id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28bb",
       title: "Valor total em estoque",
-      content: "R$ 1000,00",
+      content: "R$ " + totalProductsValue,
     },
     {
       id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
@@ -21,12 +56,12 @@ const Home = () => {
     {
       id: "58694a0f-3da1-471f-bd96-145571e29d72",
       title: "Total de vendas",
-      content: "R$ 1000,00",
+      content: "R$ " + totalSalesValue,
     },
     {
       id: "58694a0f-3da1-471f-bd96-145571e29d73",
       title: "Vendas no mês",
-      content: "R$ 1000,00",
+      content: "R$" + totalSalesCurrentMonth,
     },
   ];
 
@@ -67,7 +102,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     width: "100%",
-    padding: 10
+    padding: 10,
   },
   itemText: {
     fontSize: 20,
